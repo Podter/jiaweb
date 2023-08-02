c2b792df6ddd5c48248fe19b6e11ee373aa15b85
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-02 22:53:54 +0700

    uses swc instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
