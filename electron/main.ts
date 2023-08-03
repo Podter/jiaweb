20d2d4349cd20cb9dab05a20ed888299cbbc12fa
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-03 23:24:17 +0700

    Update main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -8,7 +8,6 @@ process.env.PUBLIC = app.isPackaged
   : path.join(process.env.DIST, "../public");
 
 let win: BrowserWindow | null;
-// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
 const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 
 function createWindow() {
@@ -23,15 +22,9 @@ function createWindow() {
     autoHideMenuBar: true,
   });
 
-  // Test active push message to Renderer-process.
-  win.webContents.on("did-finish-load", () => {
-    win?.webContents.send("main-process-message", new Date().toLocaleString());
-  });
-
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL);
   } else {
-    // win.loadFile('dist/index.html')
     win.loadFile(path.join(process.env.DIST, "index.html"));
   }
 
