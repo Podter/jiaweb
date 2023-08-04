ffa8fd538f46d61d81eb0ac62eca74d9c7036da5
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-04 20:03:02 +0700

    Update Tab.tsx

--- src/components/TabBar/Tab.tsx
+++ src/components/TabBar/Tab.tsx
@@ -1,17 +1,17 @@
-import { forwardRef, type ButtonHTMLAttributes } from "react";
+import { forwardRef, type HTMLAttributes } from "react";
 import { cn } from "@/lib/utils.ts";
 import { Button } from "@/components/ui/button.tsx";
 import { Dismiss12Regular, DocumentRegular } from "@fluentui/react-icons";
 
-interface TabProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface TabProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
   favicon?: string;
   selected?: boolean;
 }
 
-const Tab = forwardRef<HTMLButtonElement, TabProps>(
+const Tab = forwardRef<HTMLDivElement, TabProps>(
   ({ title, favicon, selected, className, ...props }, ref) => (
-    <button
+    <div
       className={cn(
         "inline-flex h-full w-full justify-center items-center border-r border-border relative transition-colors hover:bg-accent/50 hover:text-foreground group text-sm",
         !selected && "bg-accent text-accent-foreground",
@@ -20,20 +20,22 @@ const Tab = forwardRef<HTMLButtonElement, TabProps>(
       ref={ref}
       {...props}
     >
-      {favicon ? (
-        <img className="h-3 w-3 mr-2" src={favicon} alt={title} />
-      ) : (
-        <DocumentRegular scale={12} className="mr-2" />
-      )}
-      {title}
+      <button className="flex justify-center items-center h-full w-full">
+        {favicon ? (
+          <img className="h-3 w-3 mr-2" src={favicon} alt={title} />
+        ) : (
+          <DocumentRegular scale={12} className="mr-2" />
+        )}
+        {title}
+      </button>
       <Button
         variant="ghost"
         size="sm"
-        className="absolute right-2 h-6 w-6 p-0 hover:bg-muted-foreground/20 hidden group-hover:inline-flex"
+        className="h-6 w-6 p-0 hover:bg-muted-foreground/20 absolute right-2 hidden group-hover:inline-flex"
       >
         <Dismiss12Regular />
       </Button>
-    </button>
+    </div>
   ),
 );
 Tab.displayName = "Tab";
