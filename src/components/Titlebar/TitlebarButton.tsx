3688aeabab2e48bc0bff6be57f8b427b18440d6f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-04 18:33:44 +0700

    Update TitlebarButton.tsx

--- src/components/Titlebar/TitlebarButton.tsx
+++ src/components/Titlebar/TitlebarButton.tsx
@@ -1,18 +1,21 @@
+import { forwardRef, type ButtonHTMLAttributes } from "react";
 import styles from "./Titlebar.module.css";
 import { cn } from "@/lib/utils";
 
-export default function TitlebarButton({
-  className,
-  ...props
-}: JSX.IntrinsicElements["button"]) {
-  return (
-    <button
-      {...props}
-      className={cn(
-        "inline-flex rounded-md h-10 w-10 justify-center items-center transition-colors hover:bg-accent hover:text-accent-foreground",
-        styles.nodrag,
-        className,
-      )}
-    />
-  );
-}
+const TitlebarButton = forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement>
+>(({ className, ...props }, ref) => (
+  <button
+    className={cn(
+      "inline-flex rounded-md h-10 w-10 justify-center items-center transition-colors hover:bg-accent hover:text-accent-foreground",
+      styles.nodrag,
+      className,
+    )}
+    ref={ref}
+    {...props}
+  />
+));
+TitlebarButton.displayName = "TitlebarButton";
+
+export default TitlebarButton;
