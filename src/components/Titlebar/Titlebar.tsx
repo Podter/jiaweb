bf4729f536f82f0e1bd83f900ab71dad9ac6ac4e
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-04 19:04:47 +0700

    Update Titlebar.tsx

--- src/components/Titlebar/Titlebar.tsx
+++ src/components/Titlebar/Titlebar.tsx
@@ -8,7 +8,7 @@ export default function Titlebar() {
   return (
     <div
       className={cn(
-        "h-12 bg-background select-none flex fixed top-0 left-0 right-0 border-b border-border z-40",
+        "h-12 bg-background flex fixed top-0 left-0 right-0 border-b border-border z-40",
         styles.titlebar,
       )}
     >
