bd826e9c745a2032b41c9215a9c0a3037d4d8bdb
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-04 17:48:45 +0700

    added search input

---
+++ src/components/Titlebar/SearchInput.tsx
@@ -0,0 +1,14 @@
+import styles from "./Titlebar.module.css";
+import { cn } from "@/lib/utils.ts";
+import { Input } from "@/components/ui/input.tsx";
+
+export default function SearchInput() {
+  return (
+    <div className="flex justify-center items-center absolute top-0 bottom-0 left-0 right-0">
+      <Input
+        className={cn("w-[30rem] z-50 h-8 text-center", styles.nodrag)}
+        placeholder="Search or enter web address"
+      />
+    </div>
+  );
+}
--- src/components/Titlebar/Titlebar.tsx
+++ src/components/Titlebar/Titlebar.tsx
@@ -8,6 +8,7 @@ import {
 } from "@fluentui/react-icons";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils.ts";
+import SearchInput from "@/components/Titlebar/SearchInput.tsx";
 
 const { appWindow } = window;
 
@@ -30,24 +31,27 @@ export default function Titlebar() {
   return (
     <div
       className={cn(
-        "h-10 bg-background select-none flex fixed top-0 left-0 right-0 justify-between items-center border-b border-border z-40",
+        "h-12 bg-background select-none flex fixed top-0 left-0 right-0 border-b border-border z-40",
         styles.titlebar,
       )}
     >
-      <div />
-      <div className="flex justify-end">
-        <TitlebarButton onClick={() => appWindow.minimize()}>
-          <Subtract16Regular />
-        </TitlebarButton>
-        <TitlebarButton onClick={() => appWindow.toggleMaximize()}>
-          {maximized ? <SquareMultiple16Regular /> : <Maximize16Regular />}
-        </TitlebarButton>
-        <TitlebarButton
-          className="hover:text-destructive-foreground hover:bg-destructive/90"
-          onClick={() => appWindow.close()}
-        >
-          <Dismiss16Regular />
-        </TitlebarButton>
+      <div className="flex justify-between items-center w-full">
+        <div />
+        <SearchInput />
+        <div className="flex justify-end z-50">
+          <TitlebarButton onClick={() => appWindow.minimize()}>
+            <Subtract16Regular />
+          </TitlebarButton>
+          <TitlebarButton onClick={() => appWindow.toggleMaximize()}>
+            {maximized ? <SquareMultiple16Regular /> : <Maximize16Regular />}
+          </TitlebarButton>
+          <TitlebarButton
+            className="hover:text-destructive-foreground hover:bg-destructive/90"
+            onClick={() => appWindow.close()}
+          >
+            <Dismiss16Regular />
+          </TitlebarButton>
+        </div>
       </div>
     </div>
   );
