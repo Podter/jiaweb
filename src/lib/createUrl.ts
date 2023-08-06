593c1043a8b63eb5b3e20e9fc1bed84f3520fdbb
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-06 18:23:04 +0700

    Update createUrl.ts

--- src/lib/createUrl.ts
+++ src/lib/createUrl.ts
@@ -16,11 +16,27 @@ export default function createUrl(input: string): string {
     return input;
   } else if (
     parsedUrl.type !== ParseResultType.Invalid &&
-    parsedUrl.type !== ParseResultType.NotListed
+    parsedUrl.type !== ParseResultType.NotListed &&
+    input.includes(".")
   ) {
-    const parsedUrl = new URL(`https://${input}`);
-    return `${parsedUrl.protocol}//${parsedUrl.hostname}`;
+    const urlObj = new URL(`https://${input}`);
+    return `${urlObj.protocol}//${urlObj.hostname}`;
   } else {
-    return "https://www.google.com/search?q=" + encodeURIComponent(input);
+    const googleUrl =
+      "https://www.google.com/search?q=" + encodeURIComponent(input);
+
+    if (input.includes("/") && isValidUrl("https://" + input)) {
+      const urlObj = new URL(`https://${input}`);
+      const parsedHost = parseDomain(urlObj.hostname);
+      if (
+        parsedHost.type !== ParseResultType.Invalid &&
+        parsedHost.type !== ParseResultType.NotListed
+      ) {
+        return urlObj.toString();
+      } else {
+        return googleUrl;
+      }
+    }
+    return googleUrl;
   }
 }
