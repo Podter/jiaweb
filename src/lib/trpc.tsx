6d3c25ef531adf5bb68eeaf457fe87548600f59b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-09-06 22:48:45 +0700

    Update trpc.tsx

--- src/lib/trpc.tsx
+++ src/lib/trpc.tsx
@@ -1,21 +1,26 @@
 import { createTRPCReact } from "@trpc/react-query";
+import {
+  createTRPCProxyClient,
+  type CreateTRPCClientOptions,
+} from "@trpc/client";
 import { ipcLink } from "electron-trpc/renderer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, type PropsWithChildren } from "react";
 import type { AppRouter } from "../../electron/trpc/root.ts";
 
+const trpcOptions: CreateTRPCClientOptions<AppRouter> = {
+  links: [ipcLink()],
+};
+
 export const trpc = createTRPCReact<AppRouter>();
+export const trpcClient = createTRPCProxyClient<AppRouter>(trpcOptions);
 
 export function TRPCProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [ipcLink()],
-    }),
-  );
+  const [trpcReactClient] = useState(() => trpc.createClient(trpcOptions));
 
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <trpc.Provider client={trpcReactClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
   );
